c73deabb08a479192784f00962da1b861475f55b
Knist-Larsen <nicklasklarsen@gmail.com>
2020-12-09 10:58:36 +0100

    Main.js

---
+++ DDU 09-12-2020/Main.js
@@ -0,0 +1,4 @@
+var overskrifter = ["Overskrift 1", "Overskrift 2", "Overskrift 3", "Overskrift 4"];
+        for(i=0; i<overskrifter.length; i++){
+            document.getElementById("header").innerHTML = overskrifter[i];
+        }
